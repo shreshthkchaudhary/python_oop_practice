a001d31e4291969590d2dac2d49a7f8d459059a6
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-09-24 00:00:05 +0530

    adding content - 25/09/2026

--- 04_inheritance-creating_subclasses.py
+++ 04_inheritance-creating_subclasses.py
@@ -20,6 +20,7 @@ class Developer(Employee):
     def __init__ (self, first, last, pay, prog_lang):
         super().__init__(first, last, pay)
         self.prog_lang=prog_lang
+        
 
 
 class Manager(Employee):
